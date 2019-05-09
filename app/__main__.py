729602225f7b0e71d064a978a32c8eb3550c5952
Jirka-Mayer <jirkamayer97@gmail.com>
2019-05-09 23:12:08 +0200

    Organized MSER code

--- app/__main__.py
+++ app/__main__.py
@@ -13,51 +13,26 @@ segmenter = Segmenter()
 
 Output.clear()
 for item in dataset.items:
-    #if item.file != "001.jpg":
-    #    continue
-
     print(item.file)
-    Debug.show_labels(item)
+    
+    # run the segmentation algorithm
 
-    # distances
+    segmenter.segment(item.img, item.distribution())
 
-    img = segmenter.normalize(item.img)
-    img_luv = cv2.cvtColor(img, cv2.COLOR_BGR2Luv)
+    # draw the result
 
-    # via normal ROI
-
-    distances = segmenter.calculate_distance_map(img_luv, item.distribution())
-    distances_img = segmenter.distance_map_to_img(distances, img_luv)
-    Output.write_image(item.file + "_1_roi.jpg", distances_img)
-
-    # MSER sketch
-
-    img_mser = np.copy(distances_img)
-
-    delta = 5
-    min_area = 60
-    max_area = 14400
-    max_variation = 0.05
-    mser = cv2.MSER_create(delta, min_area, max_area, max_variation)
-
-    regions, bounding_boxes = mser.detectRegions(distances)
-    regions = [r * segmenter.stride for r in regions]
-
-    if len(regions) == 0:
+    if segmenter.region is None:
+        print("No region found.")
         continue
 
-    region = regions[0]
-    hull = cv2.convexHull(region)[:,0,:]
-    
-    for p in region:
-        cv2.circle(img_mser, tuple(p), segmenter.stride // 2, (0, 0, 255), 2)
-    cv2.polylines(img_mser, [hull], True, (255, 0, 255), 5)
-
-    Output.write_image(item.file + "_2_mser.jpg", img_mser)
-
-
-    outline_img = np.copy(img)
-    scale_factor = img.shape[1] / item.img.shape[1]
-    cv2.polylines(img, np.array([item.quad.to_polyline() * scale_factor], dtype=np.int32), True, (0, 0, 255), 5)
+    hull = cv2.convexHull(segmenter.region)[:,0,:]
+    img = segmenter.draw_region_over(segmenter.img_normalized, segmenter.region)
     cv2.polylines(img, [hull], True, (255, 0, 255), 5)
-    Output.write_image(item.file + "_2_outline.jpg", img)
+
+    #Debug.show_labels(item)
+    Output.write_image(item.file + "_1_result.jpg", img)
+
+    # Need to solve:
+    # - polygon to quad
+    # - initial distribution without providing it explicitly
+    # - why it fails for 011.jpg and 012.jpg
--- app/segmenter.py
+++ app/segmenter.py
@@ -11,17 +11,27 @@ class Segmenter:
         # distance map
         self.window_size = 10 # px
         self.stride = 10 # px
+
+        # intermediates for debugging
+        self.img_normalized = None
+        self.distances_img = None
+        self.region = None
     
-    def segment(self, img):
+    def segment(self, img, target_distribution):
         """Returns a quad or None if segmentation failed"""
-        pass
+        img = self.normalize(img)
+        img_luv = cv2.cvtColor(img, cv2.COLOR_BGR2Luv)
+        distances = self.calculate_distance_map(img_luv, target_distribution)
+        region = self.get_receipt_pixels(distances)
+        # TODO
 
     def normalize(self, img):
         """Returns the image with the normalized widht"""
         target_width = self.normalized_width
         scale_factor = target_width / img.shape[1]
         target_height = int(img.shape[0] * scale_factor)
-        return cv2.resize(img, (target_width, target_height))
+        self.img_normalized = cv2.resize(img, (target_width, target_height))
+        return self.img_normalized
 
     def calculate_distance_map(self, img_luv, target_distribution):
         """Calculates a distance map by sliding a window by a given stride"""
@@ -58,9 +68,38 @@ class Segmenter:
 
                 distances[y, x] = int(distance)
 
+        # DEBUG
+        self.distances_img = self.distance_map_to_img(distances, img_luv)
+
         return distances
 
     def distance_map_to_img(self, distances, img):
         """Resizes the distance map back up to fit the image size"""
         distances_img = cv2.resize(distances, (img.shape[1], img.shape[0]))
         return np.dstack([distances_img, distances_img, distances_img])
+
+    def get_receipt_pixels(self, distances):
+        """Returns a list of points (pixel positions)
+            that are the receipt or None"""
+        delta = 5
+        min_area = 60
+        max_area = 14400
+        max_variation = 0.05
+        mser = cv2.MSER_create(delta, min_area, max_area, max_variation)
+
+        regions, bounding_boxes = mser.detectRegions(distances)
+        regions = [r * self.stride for r in regions]
+
+        if len(regions) == 0:
+            self.region = None
+            return None
+
+        self.region = regions[0]
+        return regions[0]
+
+    def draw_region_over(self, img, region):
+        """Draws a region over a given image"""
+        img = np.copy(img)
+        for p in region:
+            cv2.circle(img, tuple(p), self.stride // 2, (0, 0, 255), 1)
+        return img
